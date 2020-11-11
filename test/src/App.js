8a9f9673147e148aa9825ce39839da10aa383e13
ahmedgoddi <ahmedmouda09@gmail.com>
2020-11-11 12:32:47 +0100

    first commit

---
+++ test/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Code from './JSX Checkpoint/jsx'
+import Video from './JSX Checkpoint/video'
+
+
+function App() {
+  return(
+    <div>
+    <Code/>
+    
+    <Video/>
+    </div>
+  )
+
+  
+}
+
+export default App;
---
+++ test/src/JSX Checkpoint/jsx.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Img1 from '../image/logo-2582748_1280.png'
+import './style.css'
+
+
+
+
+
+
+const myStyle = {
+    border : "solid 1px black",
+    maxWidth : "100vw"
+}
+
+
+const Code = () =>{
+    return (
+        <div style={myStyle}>
+            <h1 className ="title-red">Your name here</h1>
+            <br/>
+            <img className="imag1" src={Img1} alt="react-logo"/>
+            <br/>
+            <img className="imag1" src="CSS3_logo_and_wordmark.svg.png" alt="react-logo2"/>           
+        </div>
+    )
+}
+
+
+export default Code 
